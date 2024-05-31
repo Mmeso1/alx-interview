dd1f1290410698e2bb1ad1440532813bd78792e7
Mmeso1 <anisiubammesoma8@gmail.com>
2024-05-31 02:33:52 +0100

    task 1-

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,12 +10,16 @@ def pascal_triangle(n):
     '''
 
     if n <= 0:
-        return []
-
-    triangle = [[1] * (i + 1) for i in range(n)]
-
+        return [] 
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    triangle = [[1], [1, 1]]
     for i in range(2, n):
+        row = [1]
         for j in range(1, i):
-            triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
-
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
     return triangle

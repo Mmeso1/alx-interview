7b004b33a2fabba09520b8e44941690c05977dd3
Mmeso1 <anisiubammesoma8@gmail.com>
2024-06-20 18:06:35 +0100

    checking checker issues 1

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-"""Log parsing"""
+'''Log parsing'''
 import sys
 import re
 
-status = {"200": 0, "301": 0, "400": 0, "401": 0,
-            "403": 0, "404": 0, "405": 0, "500": 0}
-total_size = 0
-line_count = 0
-pattern = r"\d{3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[.*\] \"GET /projects/260 HTTP/1.1\" (\d{3}) (\d{1,3})"
-
 def print_stats():
-    """Print statistics."""
+    '''Print statistics.'''
     print("File size: {}".format(total_size))
     for key, value in sorted(status.items()):
         if value != 0:
@@ -18,7 +12,13 @@ def print_stats():
 
 
 if __name__ == "__main__":
-    """Main function."""
+    '''Main function.'''
+    status = {"200": 0, "301": 0, "400": 0, "401": 0,
+            "403": 0, "404": 0, "405": 0, "500": 0}
+    total_size = 0
+    line_count = 0
+    pattern = r"\d{3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[.*\] \"GET /projects/260 HTTP/1.1\" (\d{3}) (\d{1,3})"
+
     try:
         for line in sys.stdin:
             match = re.match(pattern, line)

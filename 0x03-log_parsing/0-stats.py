f08cd9fd8b53a9d1739cb9170b96825459633a2d
Mmeso1 <anisiubammesoma8@gmail.com>
2024-06-20 18:18:39 +0100

    task 0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,37 @@
 #!/usr/bin/python3
-'''
-    Log Parsing.
-'''
+'''Log parsing'''
 
+import sys
 import re
-from sys import stdin
 
 
-def print_logs_stats(file_size, stats):
-    '''
-        Prints the generated statistics.
-    '''
-    print(f'File size: {file_size}')
-    for code, count in sorted(stats.items()):
-        print(f'{code}: {count}')
+def print_stats():
+    '''Print statistics.'''
+    print("File size: {}".format(total_size))
+    for key, value in sorted(status.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
 
 
-if __name__ == '__main__':
-    n = 0
-    file_size = 0
-    stats = {}
-    pattern = re.compile(r'''
-        \d+\.\d+\.\d+\.\d+ # ip address
-        \s-\s\[(.*?)\]\s # date
-        "GET\s/projects/260\sHTTP/1.1"\s # request line
-        (?P<status_code>\d+)\s # status code
-        (?P<file_size>\d+) # file size
-    ''', re.VERBOSE)
+if __name__ == "__main__":
+    '''Main function.'''
+    status = {"200": 0, "301": 0, "400": 0, "401": 0,
+            "403": 0, "404": 0, "405": 0, "500": 0}
+    total_size = 0
+    line_count = 0
+    pattern = r"\d{3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[.*\] \"GET /projects/260 HTTP/1.1\" (\d{3}) (\d{1,3})"
+
     try:
-        for line in stdin:
-            match = pattern.match(line)
+        for line in sys.stdin:
+            match = re.match(pattern, line)
             if match:
-                file_size += int(match.group('file_size'))
-                status_code = match.group('status_code')
-                if status_code in stats:
-                    stats[status_code] += 1
-                else:
-                    stats[status_code] = 1
-                n += 1
-            if n % 10 == 0:
-                print_logs_stats(file_size, stats)
-
-    except (KeyboardInterrupt, EOFError):
-        print_logs_stats(file_size, stats)
+                status_code = match.group(1)
+                file_size = match.group(2)
+                line_count += 1
+                if status_code in status:
+                    status[status_code] += 1
+                total_size += int(file_size)
+            if line_count % 10 == 0:
+                print_stats()
+    except KeyboardInterrupt:
+        print_stats()

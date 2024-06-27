0cbce3aed2ac5b140a7c40937dd692cde9c409c3
Mmeso1 <anisiubammesoma8@gmail.com>
2024-06-27 23:30:47 +0100

    done 4 noe

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+    ALX Interview - UTF-8 Validation.
+'''
+
+
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+        data (List[int]): List of integers representing the UTF-8 data.
+
+    Returns:
+        bool: True if data is a valid UTF-8 encoding, else False.
+    """
+    count = 0
+
+    for num in data:
+        if count == 0:
+            if (num >> 5) == 0b110:
+                count = 1
+            elif (num >> 4) == 0b1110:
+                count = 2
+            elif (num >> 3) == 0b11110:
+                count = 3
+            elif (num >> 7):
+                return False
+        else:
+            if (num >> 6) != 0b10:
+                return False
+            count -= 1
+
+    return count == 0
